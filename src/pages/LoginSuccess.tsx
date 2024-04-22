57167e1d5018a5f6f1d3512ae76c7930d6a83f22
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-04-22 20:48:31 +0900

    update login page

--- src/pages/LoginSuccess.tsx
+++ src/pages/LoginSuccess.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginSuccess = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate("/");
+    })
   return (
     <div>LoginSuccess</div>
   )
