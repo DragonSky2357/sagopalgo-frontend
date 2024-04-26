8a58f0487efefc74cc1c5ff9c7e58255345d8846
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-04-26 16:05:21 +0900

    update search bar

--- src/components/MainPostFilter.tsx
+++ src/components/MainPostFilter.tsx
@@ -159,6 +159,14 @@ const MainPostFilter: React.FC<MainPostFilterProps> = ({
             onOpen={()=>setOpen(true)}
             onClose={()=>setOpen(false)}
             getOptionLabel={(option:ISearch) => option.name}
+            renderInput={(params) => (
+              <InputBase
+                {...params}
+                placeholder="검색하기"
+                inputProps={{ "aria-label": "search" }}
+                onChange={(e) => searchTitle(e.target.value)}
+              />
+            )}
             renderOption={(props, option) => (
               <Accordion key={option.id}>
                 <AccordionSummary
